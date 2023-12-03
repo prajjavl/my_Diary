aaf1c0bfc1e0702ae1e4343b4fd8d05aac4cd4e8
Prajjval dwivedi <prajjvaldwivedi2000@gmail.com>
2023-12-03 21:58:42 +0530

    created header footer about and home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/modules/nav-bar/header/header.component';
+import { FooterComponent } from './components/modules/nav-bar/footer/footer.component';
+import { HomeComponent } from './components/modules/home/home.component';
+import { AboutComponent } from './components/modules/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
